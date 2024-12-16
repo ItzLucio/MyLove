bb7435cd59d607fdda9401af3992936e194c6d1a
ItzLucio <lucio.tagliabracci@gmail.com>
2024-12-16 22:27:59 +0100

    refactor: Website structure

--- js/app.js
+++ js/app.js
@@ -1,44 +1,44 @@
-function formatDuration(date1, date2) {
-    const diffMs = Math.abs(date2 - date1);
-    const seconds = Math.floor(diffMs / 1000) % 60;
-    const minutes = Math.floor(diffMs / (1000 * 60)) % 60;
-    const hours = Math.floor(diffMs / (1000 * 60 * 60)) % 24;
-    const days = Math.floor(diffMs / (1000 * 60 * 60 * 24));
+// function formatDuration(date1, date2) {
+//     const diffMs = Math.abs(date2 - date1);
+//     const seconds = Math.floor(diffMs / 1000) % 60;
+//     const minutes = Math.floor(diffMs / (1000 * 60)) % 60;
+//     const hours = Math.floor(diffMs / (1000 * 60 * 60)) % 24;
+//     const days = Math.floor(diffMs / (1000 * 60 * 60 * 24));
 
-    return `${days} giorni, ${hours} ore, ${minutes} minuti e ${seconds} secondi`;
-}
+//     return `${days} giorni, ${hours} ore, ${minutes} minuti e ${seconds} secondi`;
+// }
 
-document.addEventListener("DOMContentLoaded", () => {
-    const all = document.querySelector("#all")
-    const countdown = document.querySelector("#countdown")
+// document.addEventListener("DOMContentLoaded", () => {
+//     const all = document.querySelector("#all")
+//     const countdown = document.querySelector("#countdown")
 
-    const countdownText = document.querySelector(".countdown-text")
+//     const countdownText = document.querySelector(".countdown-text")
 
-    const nowDateMS_FirstCheck = new Date().getTime()
-    const thatDateMS_FirstCheck = new Date("2024-12-17T00:00:00").getTime()
+//     const nowDateMS_FirstCheck = new Date().getTime()
+//     const thatDateMS_FirstCheck = new Date("2024-12-17T00:00:00").getTime()
 
-    if (thatDateMS_FirstCheck > nowDateMS_FirstCheck) {
-        all.classList.add('hidden')
-        countdown.classList.remove('hidden')
+//     if (thatDateMS_FirstCheck > nowDateMS_FirstCheck) {
+//         all.classList.add('hidden')
+//         countdown.classList.remove('hidden')
 
-        countdownText.textContent = formatDuration(nowDateMS_FirstCheck, thatDateMS_FirstCheck)
-    } else {
-        all.classList.remove('hidden')
-        countdown.classList.add('hidden')
-    }
+//         countdownText.textContent = formatDuration(nowDateMS_FirstCheck, thatDateMS_FirstCheck)
+//     } else {
+//         all.classList.remove('hidden')
+//         countdown.classList.add('hidden')
+//     }
 
-    setInterval(function() {
-        const nowDateMS = new Date().getTime()
-        const thatDateMS = new Date("2024-12-17T00:00:00").getTime()
+//     setInterval(function() {
+//         const nowDateMS = new Date().getTime()
+//         const thatDateMS = new Date("2024-12-17T00:00:00").getTime()
 
-        if (thatDateMS > nowDateMS) {
-            all.classList.add('hidden')
-            countdown.classList.remove('hidden')
+//         if (thatDateMS > nowDateMS) {
+//             all.classList.add('hidden')
+//             countdown.classList.remove('hidden')
 
-            countdownText.textContent = formatDuration(nowDateMS, thatDateMS)
-        } else {
-            all.classList.remove('hidden')
-            countdown.classList.add('hidden')
-        }
-    }, 1000)
-})
+//             countdownText.textContent = formatDuration(nowDateMS, thatDateMS)
+//         } else {
+//             all.classList.remove('hidden')
+//             countdown.classList.add('hidden')
+//         }
+//     }, 1000)
+// })
